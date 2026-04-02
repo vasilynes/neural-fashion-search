58bf9c4bf77034fcded98b1f9e0df077a8f7badb
vasilynes <ivyylas@gmail.com>
2026-04-02 18:28:36 +0700

    Searching by text is refactored, now we can search by a single text query, a batch of texts or a batch of embeddings

--- app/services/search.py
+++ app/services/search.py
@@ -1,4 +1,4 @@
-from qdrant_client.models import Fusion, FusionQuery
+from qdrant_client.models import Fusion, FusionQuery, QueryRequest, PrefetchQuery
 from app.services.model import ModelService
 from app.config import config
 
@@ -7,19 +7,8 @@ class SearchService:
         self.client = client
         self.model_service = model_service
 
-    def search_by_text(self, query, limit=10):
-        dense = self.model_service.embed_text([query])[0]
-        sparse = self.model_service.embed_text_sparse([query])[0]
-
-        return self.client.query_points(
-            collection_name=config.DB_NAME,
-            prefetch=[
-                {'query': dense, 'using': 'text', 'limit': limit * 2},
-                {'query': sparse, 'using': 'text_sparse', 'limit': limit * 2},
-            ],
-            query=FusionQuery(fusion=Fusion.RRF),
-            limit=limit
-        )
+    def search_by_text(self, query: str, limit=10):
+        return self.search_by_text_batch([query], limit=limit)[0]
 
     def search_by_image(self, image, limit=10):
         dense = self.model_service.embed_image(image)
@@ -30,3 +19,31 @@ class SearchService:
             using='image',
             limit=limit
         )
+    
+    def search_by_text_batch(self, queries: list[str], limit=10):
+        dense_batch = self.model_service.embed_text(queries)
+        sparse_batch = self.model_service.embed_text_sparse(queries)
+        return self._search_by_embeddings(dense_batch, sparse_batch, limit)
+
+    def search_by_embeddings(self, dense: list, sparse: list, limit=10):
+        return self._search_by_embeddings([dense], [sparse], limit)[0]
+
+    def _search_by_embeddings(self, dense_batch: list, sparse_batch: list, limit=10):
+        requests = [
+            QueryRequest(
+                prefetch=[
+                    PrefetchQuery(query=dense, using='text', limit=limit * 2),
+                    PrefetchQuery(query=sparse, using='text_sparse', limit=limit * 2),
+                ],
+                query=FusionQuery(fusion=Fusion.RRF),
+                limit=limit
+            )
+            for dense, sparse in zip(dense_batch, sparse_batch)
+        ]
+
+        return self.client.query_batch_points(
+            collection_name=config.DB_NAME,
+            requests=requests,
+        )
+    
+
